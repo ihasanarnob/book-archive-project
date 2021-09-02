de6d9188d13afff9fcf8ea9d1c099e36c97d57d0
ihasanarnob <hasanarnob3@gmail.com>
2021-09-02 19:02:21 +0600

    final commit

--- js/book.js
+++ js/book.js
@@ -3,6 +3,8 @@ const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value = '';
+    const clearReaults = document.getElementById('search-quantity');
+    clearReaults.textContent = '';
     const url = `http://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
         .then(res => res.json())
@@ -28,26 +30,25 @@ const displayBookInfo = infos => {
     else {
         const errorClear = document.getElementById('book-not-found');
         errorClear.textContent = '';
+        const searchQuantity = document.getElementById('search-quantity');
+        const div1 = document.createElement('div');
+        div1.innerHTML = `
+        <h4> ${infos.length} </span> results found </h4>
+        `;
+        searchQuantity.appendChild(div1);
         infos?.forEach(info => {
-            /*   console.log(info.title_suggest.length);
-              const searchQuantity = document.getElementById('search-quantity');
-              const div1 = document.createElement('div');
-              div1.innerHTML = `
-              <h4> ${info.length} results found </h4>
-              `;
-              searchQuantity.appendChild(div1); */
 
 
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
                        <div class="card h-100">
-                           <img src="https://covers.openlibrary.org/b/id/${info.cover_i}-M.jpg" class="card-img-top img-thumbnail" alt="...">
+                           <img src="https://covers.openlibrary.org/b/id/${info.cover_i}-M.jpg" class="card-img-top img-thumbnail h-50" alt="...">
                            <div class="card-body">
                                <h5 class="card-title">Book Name : ${info.title}</h5>
                                <h6 class="card-title"> Author : ${info.author_name ? info.author_name : ''}</h6>
                                <p class="card-text">First Publish Year : ${info.first_publish_year ? info.first_publish_year : ''}</p>
-                               <p class="card-text">Publisher : ${info.publisher ? info.publisher : ''}</p>
+                               <p class="card-text">Publisher : ${info.publisher ? info.publisher.slice(0, 20) : ''}</p>
                            </div>
                        </div>
                    `;
