44e39186cb7ced6e9e9a4f4b513372cdc96c844b
ihasanarnob <hasanarnob3@gmail.com>
2021-09-02 15:44:22 +0600

    search error add

--- js/book.js
+++ js/book.js
@@ -10,27 +10,49 @@ const searchBook = () => {
 
 }
 // show book Informations
-
 const displayBookInfo = infos => {
     const divContainer = document.getElementById('book-info');
     divContainer.textContent = '';
     // console.log(infos);
-    infos?.forEach(info => {
-        console.log(info);
+    // not found Eorror
+    if (infos.length === 0) {
+        const bookNotFound = document.getElementById('book-not-found');
         const div = document.createElement('div');
-        div.classList.add('col');
+
+        div.classList.add('text-danger');
         div.innerHTML = `
-                   <div class="card h-100">
-                       <img src="https://covers.openlibrary.org/b/id/${info.cover_i}-M.jpg" class="card-img-top img-thumbnail" alt="...">
-                       <div class="card-body">
-                           <h5 class="card-title">Book Name : ${info.title}</h5>
-                           <h6 class="card-title"> Author : ${info.author_name ? info.author_name : ''}</h6>
-                           <p class="card-text">First Publish Year : ${info.first_publish_year ? info.first_publish_year : ''}</p>
-                           <p class="card-text">Publisher : ${info.publisher ? info.publisher : ''}</p>
+        <h4>Sorry! Search result not found. </h4>
+        `;
+        bookNotFound.appendChild(div);
+    }
+    else {
+        const errorClear = document.getElementById('book-not-found');
+        errorClear.textContent = '';
+        infos?.forEach(info => {
+            /*   console.log(info.title_suggest.length);
+              const searchQuantity = document.getElementById('search-quantity');
+              const div1 = document.createElement('div');
+              div1.innerHTML = `
+              <h4> ${info.length} results found </h4>
+              `;
+              searchQuantity.appendChild(div1); */
+
+
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+                       <div class="card h-100">
+                           <img src="https://covers.openlibrary.org/b/id/${info.cover_i}-M.jpg" class="card-img-top img-thumbnail" alt="...">
+                           <div class="card-body">
+                               <h5 class="card-title">Book Name : ${info.title}</h5>
+                               <h6 class="card-title"> Author : ${info.author_name ? info.author_name : ''}</h6>
+                               <p class="card-text">First Publish Year : ${info.first_publish_year ? info.first_publish_year : ''}</p>
+                               <p class="card-text">Publisher : ${info.publisher ? info.publisher : ''}</p>
+                           </div>
                        </div>
-                   </div>
-               `;
-        divContainer.appendChild(div);
-    });
+                   `;
+            divContainer.appendChild(div);
+        });
+    }
 }
 
