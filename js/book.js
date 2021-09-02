f330cd4e59c95e77748b02829bf5fe3e2d843ee5
ihasanarnob <hasanarnob3@gmail.com>
2021-09-02 12:52:03 +0600

    standard file

--- js/book.js
+++ js/book.js
@@ -1,30 +1,36 @@
-// console.log('book is on');
+// search box and button
+const searchBook = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    searchField.value = '';
+    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayBookInfo(data.docs));
 
+}
 // show book Informations
-const url = `http://openlibrary.org/search.json?q=javascript`;
-fetch(url)
-    .then(res => res.json())
-    .then(data => displayBookInfo(data.docs));
 
 const displayBookInfo = infos => {
     const divContainer = document.getElementById('book-info');
-    infos.forEach(info => {
+    divContainer.textContent = '';
+    // console.log(infos);
+    infos?.forEach(info => {
         console.log(info);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
                    <div class="card h-100">
-                       <img src="..." class="card-img-top" alt="...">
+                       <img src="https://covers.openlibrary.org/b/id/${info.cover_i}-M.jpg" class="card-img-top img-thumbnail" alt="...">
                        <div class="card-body">
-                           <h5 class="card-title">Book Name:${info.title}</h5>
-                           <h6 class="card-title"> Author:${info?.author_name[0]}</h6>
-                           <p class="card-text">Publish Year:${info.publish_year[0]}</p>
-                           <p class="card-text">First Publish:${info.publish_date[0]}</p>
-                           <p class="card-text">Publisher:${info.publisher[0]}</p>
+                           <h5 class="card-title">Book Name : ${info.title}</h5>
+                           <h6 class="card-title"> Author : ${info.author_name ? info.author_name : ''}</h6>
+                           <p class="card-text">First Publish Year : ${info.first_publish_year ? info.first_publish_year : ''}</p>
+                           <p class="card-text">Publisher : ${info.publisher ? info.publisher : ''}</p>
                        </div>
                    </div>
                `;
         divContainer.appendChild(div);
     });
 }
-displayBookInfo();
+
